44596501e7239eb2cfd30329027904d06d52e890
Sidharth Chakrat <231430131014@gtu.edu.in>
2026-08-04 21:21:27 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        vector<int> a(26,0);
+        for(int i = 0; i < s.size(); i++){
+            a[s[i]-'a']++;
+            a[t[i]-'a']--;
+        }
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] != 0){
+                return false;
+            }
+        }   
+        return true;
+    }
+};
